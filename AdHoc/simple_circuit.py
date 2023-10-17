eba8a7af3d7098aee037046da22a5d5ee39b0645
AnurajT <anurajtalati@gmail.com>
2023-10-17 23:34:21 +1100

    Update simple_circuit.py

--- AdHoc/simple_circuit.py
+++ AdHoc/simple_circuit.py
@@ -1,5 +1,6 @@
 import pennylane as qml
 from pennylane import numpy as np
+import matplotlib.pyplot as plt
 
 dev = qml.device("default.qubit", wires=2)
 
@@ -13,4 +14,14 @@ def circuit(theta):
 
     return qml.expval(qml.PauliZ(wires=0))
 
-print(circuit(np.pi))
+# print(circuit(np.pi))
+
+thetas = np.arange(-np.pi, np.pi, 0.01)
+
+measurements = np.zeros(len(thetas))
+
+for i, theta in enumerate(thetas):
+    measurements[i] = circuit(theta)
+
+plt. plot(thetas, measurements)
+plt.show()
